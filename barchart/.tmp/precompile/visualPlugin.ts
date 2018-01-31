8fe79ba38cdf727f8508e497ecd19b4a203bc91d
mohskhan <mohskhan@bmc.com>
2018-01-31 15:05:39 +0530

    0.1

---
+++ barchart/.tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var barchart36D4B60A560147C4BF611C9574D518F9_DEBUG = {
+        name: 'barchart36D4B60A560147C4BF611C9574D518F9_DEBUG',
+        displayName: 'barchart',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.9.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.barchart36D4B60A560147C4BF611C9574D518F9.Visual(options),
+        custom: true
+    };
+}
